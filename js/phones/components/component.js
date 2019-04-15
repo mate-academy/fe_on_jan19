5e732ec0dd9983efc3f663e6e9418d4bf47e9d04
Aleksandr Chihariov <kamzed1995@gmail.com>
2019-04-15 16:04:07 +0300

    emit/subscribe was implemented property

--- js/phones/components/component.js
+++ js/phones/components/component.js
@@ -1,40 +1,51 @@
 'use strict';
 
 export default class Component {
-	constructor({ element }) {
-		this._element = element;
-    this._callbackMap = {}; /////////////////////////////////
-	}
+  constructor({ element }) {
+    this._callbackMap = {};
+    this._element = element;
+  }
 
-	 on(eventName, selector, callback) {
+  on(eventName, selector, callback) {
     this._element.addEventListener(eventName, (event) => {
       const delegatedTarget = event.target.closest(selector);
       if (!delegatedTarget) {
-          return;
+        return;
       }
       callback(event);
     });
-   }
-
-
-   emit(eventName, data) {
-      const callback = this._callbackMap[eventName];
-      if(!callback) {
-        return;
-      }
-      callback(data);
-    }
-
-  subscribe(eventName, callback) {
-    this._callbackMap[eventName] = callback;
   }
 
+  emit(eventName, data) {
+    const callbacks = this._callbackMap[eventName];
+    if (!callbacks) {
+      return;
+    }
+    callbacks.forEach((callback) => {
+      callback(data);
+    })
+  }
 
-	hide() {
-      this._element.hidden = true;
+  subscribe(eventName, callback) {
+    if (!this._callbackMap[eventName]) {
+      this._callbackMap[eventName] = [];
+    }
+    this._callbackMap[eventName].push(callback);
+  }
+
+  unsubscribe(eventName, callbackToRemove) {
+    const callbacks = this._callbackMap[eventName];
+    if (callbacks) {
+      this._callbackMap[eventName] = callbacks
+      .filter((cb) => cb !== callbackToRemove)
+    }
+  }
+
+  hide() {
+    this._element.hidden = true;
   }
 
   show() {
-  	this._element.hidden = false;
+    this._element.hidden = false;
   }
 }
