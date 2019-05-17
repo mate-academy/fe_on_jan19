174a0925258ac1548347cadb0302f1c8a041b879
Svet <39680564+SvitlanaTsupryk-jul18@users.noreply.github.com>
2019-05-17 13:52:52 +0300

    Update phones-catalog.js

--- js/phones/components/phones-catalog.js
+++ js/phones/components/phones-catalog.js
@@ -1,62 +1,67 @@
 import Component from './component.js';
 
 export default class PhonesCatalog extends Component {
-    constructor({
-        element,
-        phones = []
-    }) {
-        super({
-            element
-        });
-        this._phones = phones;
+  constructor({ element }) {
+    super({ element });
+    this._phones = [];
 
-        this._render();
+    this._render();
 
-        this.on('click', '[data-element="details-link"]', (event) => {
-            const phoneEl = event.target.closest('[data-element="phone-element"]');
-            const phoneId = phoneEl.dataset.phoneId;
-            this.emit('phone-selected', phoneId);
-        })
+    this.on('click', '[data-element="details-link"]', (event) => {
+      const phoneEl = event.target.closest('[data-element="phone-element"]');
+      const phoneId = phoneEl.dataset.phoneId;
+      this.emit('phone-selected', phoneId);
+    });
 
-        
-         this.on('click', '[data-element="add-to-cart"]', (event) => {
-            const phoneEl = event.target.closest('[data-element="phone-element"]');
-            const phoneId = phoneEl.dataset.phoneId;
-            this.emit('add-phone', phoneId);
-        })
-    }
+    this.on('click', '[data-element="add-to-cart"]', (event) => {
+      const phoneEl = event.target.closest('[data-element="phone-element"]');
+      const phoneId = phoneEl.dataset.phoneId;
+      this.emit('add-phone', phoneId);
+    });
+  }
 
-    _render() {
-        this._element.innerHTML = `
+  show(phones) {
+    this._phones = phones;
+    super.show();
+    this._render();
+  }
+
+  _render() {
+    this._element.innerHTML = `
         <ul class="phones">
             ${
-                this._phones.map(phone => `
-                    <li 
-                    class="thumbnail"
-                    data-element="phone-element"
-                    data-phone-id=${phone.id}
-                    >
-                        <a 
-                        href="#!/phones/motorola-xoom-with-wi-fi" 
-                        class="thumb"
-                        data-element="details-link"
-                        >
-                        <img alt="${phone.name}™ with Wi-Fi" src="${phone.imageUrl}">
-                        </a>
-                        <div class="phones__btn-buy-wrapper">
-                        <a class="btn btn-success" data-element="add-to-cart">
-                            Add
-                        </a>
-                        </div>
-                        <a 
-                        href="#!/phones/motorola-xoom-with-wi-fi"
-                        data-element="details-link"
-                        >${phone.name}</a>
-                        <p>${phone.snippet}</p>
-                    </li>
-                `).join('')
-            }
+  this._phones.map(phone => `
+            <li 
+            class="thumbnail"
+            data-element="phone-element"
+            data-phone-id=${phone.id}
+            >
+                <a 
+                href="#!/phones/motorola-xoom-with-wi-fi" 
+                class="thumb"
+                data-element="details-link"
+                >
+                <img 
+                alt="${phone.name}™" 
+                src="${phone.imageUrl}">
+                </a>
+                <div class="phones__btn-buy-wrapper">
+                <a 
+                class="btn btn-success"
+                data-element="add-to-cart"
+                >
+                    Add
+                </a>
+                </div>
+                <a 
+                href="#!/phones/motorola-xoom-with-wi-fi"
+                data-element="details-link"
+                >${phone.name}</a>
+                <p>${phone.snippet}</p>
+            </li>
+        `).join('')
+}
         </ul>
-        `
-    }
+        `;
+  }
 }
