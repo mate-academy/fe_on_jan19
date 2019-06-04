90e8737ccb9e650f03ec2ecfe5cb77b9eaa663b6
Alexandr Chigarev <kamzed1995@gmail.com>
2019-06-04 21:02:51 +0300

    fix filter

--- js/phones/services/phones-service.js
+++ js/phones/services/phones-service.js
@@ -1,14 +1,32 @@
 'use strict';
 
-const API_URL = 'https://mate-academy.github.io/phone-catalogue-static/api/';
+    
+const API_URL = 'https://mate-academy.github.io/phone-catalogue-static/api';
 
 const PhonesService = {
-  getAll({query='', order=''} = {}) {
-      return fetch(API_URL + '/phones.json').then((response) => response.json());
+  async getAll({ query = '', order = '' } = {}) {
+    let result;
+    try {
+      const response = await window.fetch(API_URL + '/phones.json');
+      result = await response.json();
+    } catch (error) {
+      result = [];
+    }
+    const filteredResult = result.filter((phone) => {
+    	return phone.name.toLowerCase().includes(query.toLowerCase());
+    });
+    switch(order) {
+    	case 'age':
+    		filteredResult.sort((a,b) => a.age - b.age);
+    		break;
+    	case 'name':
+    		filteredResult.sort((a,b) => a.name.localeCompare(b.name));
+    }
+    return filteredResult;
   },
   getById(id) {
-    return fetch(API_URL + '/phones/' + id + '.json').then((response) => response.json());
-  }
-}
-
+    return window.fetch(API_URL + '/phones/' + id + '.json')
+      .then((response) => response.json());
+  },
+};
 export default PhonesService;
